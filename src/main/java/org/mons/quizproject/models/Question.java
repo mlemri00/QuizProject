17c64f0253bcad8d16b7614de5d3b7a634e7a1a0
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-24 17:16:18 +0100

    Implementación de la classe question

---
+++ src/main/java/org/mons/quizproject/models/Question.java
@@ -0,0 +1,30 @@
+package org.mons.quizproject.models;
+
+import lombok.*;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+public class Question{
+
+    private String category;
+    private String id;
+    private String correctAnswer;
+    private List<String> incorrectAnswers;
+    private questionText question;
+    private List<String> tags;
+    private String type;
+    private String difficulty;
+    private List<String> regions;
+    private boolean isNiche;
+
+    @Data
+    @Getter
+    private class questionText{
+        String text;
+    }
+
+}
--- src/main/java/org/mons/quizproject/service/QuestionService.java
+++ src/main/java/org/mons/quizproject/service/QuestionService.java
@@ -9,12 +9,7 @@ import java.util.*;
 
 public class QuestionService {
 
-
-    public static void main(String[] args) {
-
-
-
-    }
+    public List<Question>
 
 
 }
