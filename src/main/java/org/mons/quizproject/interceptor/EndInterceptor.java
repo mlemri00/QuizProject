948427017108ffbbf8d77d27de83211fa95d25cb
Pedro <prubio82@alumnes.politecnicllevant.cat>
2025-11-25 19:26:56 +0100

    ranking funciona, feo y mal pero funciona

---
+++ src/main/java/org/mons/quizproject/interceptor/EndInterceptor.java
@@ -0,0 +1,26 @@
+package org.mons.quizproject.interceptor;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/end")
+public class EndInterceptor implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("username") == null) {
+            response.sendRedirect("login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
+
