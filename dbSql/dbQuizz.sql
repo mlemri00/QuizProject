9af630e7e4373ab292e397c7c33177dc72b6b1fe
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-26 16:52:42 +0100

    Canvi archiud sql

--- dbSql/dbQuizz.sql
+++ dbSql/dbQuizz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: dbQuiz
--- Tiempo de generación: 25-11-2025 a las 18:31:24
+-- Tiempo de generación: 26-11-2025 a las 15:48:18
 -- Versión del servidor: 8.0.44
 -- Versión de PHP: 8.3.27
 
@@ -26,25 +26,32 @@ USE `quiz`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Game`
+-- Estructura de tabla para la tabla `game`
 --
 
 CREATE TABLE `game` (
-                           `id` int NOT NULL,
-                           `user_id` int NOT NULL,
-                           `game_score` int NOT NULL
+                        `id` int NOT NULL,
+                        `user_id` int NOT NULL,
+                        `game_score` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Volcado de datos para la tabla `Game`
+-- Volcado de datos para la tabla `game`
 --
 
 INSERT INTO `game` (`id`, `user_id`, `game_score`) VALUES
-                                                          (10, 1, 4),
-                                                          (11, 1, 10),
-                                                          (12, 1, 40),
-                                                          (13, 1, 21),
-                                                          (14, 1, 20);
+                                                       (10, 1, 4),
+                                                       (11, 1, 10),
+                                                       (12, 1, 40),
+                                                       (13, 1, 21),
+                                                       (14, 1, 20),
+                                                       (15, 1, 1234),
+                                                       (16, 4, 7),
+                                                       (17, 4, 5),
+                                                       (18, 4, 10),
+                                                       (19, 4, 7),
+                                                       (20, 6, 23),
+                                                       (21, 6, 30);
 
 -- --------------------------------------------------------
 
@@ -66,17 +73,22 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `password`) VALUES
                                                                                   (1, 'pedro', 'pedro', 'rubio', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
-                                                                                  (2, 'mohanomoja', 'miau', 'chelo', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
+                                                                                  (2, 'mohanomoja', 'miau', 'chelo', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
+                                                                                  (3, 'zxcv', 'zxcv', 'zxcv', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
+                                                                                  (4, 'fghj', 'fghj', '1234', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
+                                                                                  (5, 'midass', 'midass', 'midass', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
+                                                                                  (6, 'zurullete', 'a', 'a', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `Game`
+-- Indices de la tabla `game`
 --
 ALTER TABLE `game`
-    ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_game` (`user_id`);
 
 --
 -- Indices de la tabla `users`
@@ -89,16 +101,26 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT de la tabla `Game`
+-- AUTO_INCREMENT de la tabla `game`
 --
 ALTER TABLE `game`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `game`
+--
+ALTER TABLE `game`
+    ADD CONSTRAINT `fk_user_game` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
